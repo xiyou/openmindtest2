1cb600fa26bed92b3f332d604cf0eebbfaf9dd29
rongli <944261985@qq.com>
2019-05-18 23:10:40 +0800

    增加App组件

---
+++ src/routes/App/App.js
@@ -0,0 +1,14 @@
+/**
+ * @description 该应用的根组件
+ * @author rongli
+ */
+import React from "react";
+import { connect } from "dva";
+
+function App(props) {
+  return <div>爱你</div>;
+}
+
+
+
+export default connect()(App);
